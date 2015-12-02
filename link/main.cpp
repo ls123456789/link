3ee33c7be341b9af13539119b5d00ee6857e177b
ls123456789 <2953757467@qq.com>
2015-12-02 21:15:34 +0800

    link
    先声明一个结构体student，其中包括数据域（姓名和出生年份），和一个指针域。在主函数中，先定义四个有名结构体A,B,C,D，然后分别输入它们
    数据域，再分别输出。

--- link/main.cpp
+++ link/main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <iostream>
-
+struct student
+{
+    char name[100];
+    int year;
+    student *next;
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    student A,B,C,D;
+    scanf("%s%d",A.name,&A.year);
+    scanf("%s%d",B.name,&B.year);
+    scanf("%s%d",C.name,&C.year);
+    scanf("%s%d",D.name,&D.year);
+    printf("%s %d\n",A.name,A.year);
+    printf("%s %d\n",B.name,B.year);
+    printf("%s %d\n",C.name,C.year);
+    printf("%s %d\n",D.name,D.year);
     return 0;
 }
