fc04d435d102ea528d8f624c62d1b0068ca0887f
ls123456789 <2953757467@qq.com>
2015-12-09 16:15:24 +0800

    结构体中的指针
    新定义了两个student类型的指针，将四个定义的有名结构体的输出删掉。再使每一个有名结构体的指针域指向下一个有名结构体的首地址，最后一个指针域
    值为空，使head指向第一个有名结构体的首地址，再将head的值赋给p（一会循环时使用）。
    设置一个while循环，判断p的值是否为空，如果不为空执行循环体，如果为空，循环结束。循环体共有两行，第一行用来输出p所指向的循环体的name和
    year。第二行语句时将此时p所指向的结构体中的指针域next赋给p，使p指向下一个结构体的首字母，直到p的值被赋为空。

--- link/main.cpp
+++ link/main.cpp
@@ -17,13 +17,20 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     
     student A,B,C,D;
+    student *p,*head;
     scanf("%s%d",A.name,&A.year);
     scanf("%s%d",B.name,&B.year);
     scanf("%s%d",C.name,&C.year);
     scanf("%s%d",D.name,&D.year);
-    printf("%s %d\n",A.name,A.year);
-    printf("%s %d\n",B.name,B.year);
-    printf("%s %d\n",C.name,C.year);
-    printf("%s %d\n",D.name,D.year);
+    A.next = &B;
+    B.next = &C;
+    C.next = &D;
+    D.next = 0;
+    head = &A;
+    p = head;
+    while (p!=0) {
+        printf("%s %d\n",p->name,p->year);
+        p = p->next;
+    }
     return 0;
 }
